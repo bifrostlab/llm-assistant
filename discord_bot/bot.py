151a2917f14761236dbadbdbf406662cf431dd9d
Rodo <rodonguyendd@gmail.com>
2024-02-11 13:21:41 +1000

    refactor: use module namespace

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,61 +1,52 @@
 import os
 from dotenv import load_dotenv
 import asyncio
-from interactions import (
-    AutocompleteContext,
-    Client,
-    Intents,
-    listen,
-    slash_command,
-    SlashContext,
-    SlashCommand,
-    slash_option,
-    SlashCommandChoice,
-    OptionType,
-)
+import interactions
 
-vait_guild_id = [1230000000999]  # THIS SHOULD BE ASSIGNED to scopes WHEN DEPLOYED
+# Real VAIT server id to be used and
+# THIS SHOULD BE ASSIGNED to `scopes` WHEN DEPLOYED
+vait_guild_id = [999999999]
 
-bot = Client(intents=Intents.DEFAULT)
+bot = interactions.Client(intents=interactions.Intents.DEFAULT)
 
 
-@listen()
+@interactions.listen()
 async def on_ready():
     print("Ready")
     print(f"This bot is owned by {bot.owner}")
 
 
-@listen()
+@interactions.listen()
 async def on_message_create(event):
     print(f"message received: {event.message.content}")
 
 
-@slash_command(name="my_short_command", description="My first command :)")
-async def my_short_command(ctx: SlashContext):
+@interactions.slash_command(name="my_short_command", description="My first command :)")
+async def my_short_command(ctx: interactions.SlashContext):
     await ctx.send("Hello World")
 
 
-@slash_command(name="my_long_command", description="My second command :)")
-async def my_long_command_function(ctx: SlashContext):
+@interactions.slash_command(name="my_long_command", description="My second command :)")
+async def my_long_command_function(ctx: interactions.SlashContext):
     await ctx.defer()  # use defer for time-comsuming commands
     await asyncio.sleep(600)
     await ctx.send("Hello World")
 
 
-@slash_command(name="ask", description="Ask an LLM")
-@slash_option(
+@interactions.slash_command(name="ask", description="Ask an LLM")
+@interactions.slash_option(
     name="model",
     description="Choose an LLM model",
     required=True,
-    opt_type=OptionType.STRING,
+    opt_type=interactions.OptionType.STRING,
     autocomplete=True,
 )
-async def ask_model(ctx: SlashContext, model: str):
+async def ask_model(ctx: interactions.SlashContext, model: str):
     await ctx.send(f"You asked model {model}")
 
 
 @ask_model.autocomplete("model")
-async def autocomplete(ctx: AutocompleteContext):
+async def autocomplete(ctx: interactions.AutocompleteContext):
     string_option_input = ctx.input_text  # note: can be empty
     print(f"input: {string_option_input}")
     # you can use ctx.kwargs.get("name") to get the current state of other options - note they can be empty too
