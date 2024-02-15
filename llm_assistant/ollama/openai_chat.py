3a7659dd3456bc7e94c9b265bb9cfbf360991b58
Sam Huynh <samhwang2112.dev@gmail.com>
2024-02-15 21:52:12 +1100

    chore: change to use 2 space indentations (#21)

--- llm_assistant/ollama/openai_chat.py
+++ llm_assistant/ollama/openai_chat.py
@@ -5,16 +5,16 @@ import argparse
 client = OpenAI(base_url="http://0.0.0.0:8000", api_key="FAKE")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("model")
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("model")
+  args = parser.parse_args()
 
-    response = client.chat.completions.create(
-        model=args.model,
-        messages=[{"role": "user", "content": "write a short poem"}],
-        stream=True,
-    )
+  response = client.chat.completions.create(
+    model=args.model,
+    messages=[{"role": "user", "content": "write a short poem"}],
+    stream=True,
+  )
 
-    for chunk in response:
-        # print(chunk)
-        print(chunk.choices[0].delta.content, end="", flush=True)
+  for chunk in response:
+    # print(chunk)
+    print(chunk.choices[0].delta.content, end="", flush=True)
