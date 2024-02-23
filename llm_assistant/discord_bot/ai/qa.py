fd34cc3f3ecc0a5e783cc17a51aac030d1cccbc4
Duy Hung Tran <hung.dtrn@gmail.com>
2024-02-23 12:27:38 +0700

    chore: fix typo, wrong indent, and lack blankspace

--- llm_assistant/discord_bot/ai/qa.py
+++ llm_assistant/discord_bot/ai/qa.py
@@ -1,6 +1,6 @@
 import os
 import dotenv
-from openai import OpenAI, AsyncOpenAI
+from openai import AsyncOpenAI
 
 import time
 
@@ -9,7 +9,7 @@ dotenv.load_dotenv()
 
 async def async_answer_question(model, question):
   start = time.time()
-  ai_server = os.environ.get("ai_server", "http://localhost:8000")
+  ai_server = os.environ.get("AI_SERVER", "http://localhost:8000")
   client = AsyncOpenAI(base_url=ai_server, api_key="FAKE")
   response = await client.chat.completions.create(
     model=model,
@@ -18,19 +18,3 @@ async def async_answer_question(model, question):
   out = response.choices[0].message.content
 
   return out, time.time() - start
-
-
-def answer_question(model, question):
-  ai_server = os.environ.get("ai_server", "http://localhost:8000")
-  client = OpenAI(base_url=ai_server, api_key="FAKE")
-  response = client.chat.completions.create(
-    model=model,
-    messages=[{"role": "user", "content": question}],
-  )
-  out = response.choices[0].message.content
-
-  return out
-
-
-if __name__ == "__main__":
-  print(answer_question("gpt-3.5-turbo", "Tell me about Melbourne"))
