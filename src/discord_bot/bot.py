ed77d27edd14f55f727575e9cf404da6767d7392
Rodo <rodonguyendd@gmail.com>
2024-03-01 23:52:03 +1000

    feat: add input validation for "model"

--- src/discord_bot/bot.py
+++ src/discord_bot/bot.py
@@ -4,6 +4,8 @@ import os
 import interactions
 from dotenv import load_dotenv
 
+MODEL_CHOICES = ["gpt-3.5-turbo", "gpt-4", "phi"]
+
 bot = interactions.Client(intents=interactions.Intents.DEFAULT)
 
 
@@ -18,18 +20,6 @@ async def on_message_create(event: interactions.api.events.MessageCreate) -> Non
   print(f"message received: {event.message.content}")
 
 
-@interactions.slash_command(name="my_short_command", description="My first command :)")
-async def my_short_command(ctx: interactions.SlashContext) -> None:
-  await ctx.send("Hello World")
-
-
-@interactions.slash_command(name="my_long_command", description="My second command :)")
-async def my_long_command_function(ctx: interactions.SlashContext) -> None:
-  await ctx.defer()  # use defer for time-comsuming commands
-  await asyncio.sleep(600)
-  await ctx.send("Hello World")
-
-
 @interactions.slash_command(name="ask", description="Ask an LLM")
 @interactions.slash_option(
   name="model",
@@ -42,18 +32,19 @@ async def my_long_command_function(ctx: interactions.SlashContext) -> None:
   name="prompt", description="Enter your prompt", required=True, opt_type=interactions.OptionType.STRING, min_length=10
 )
 async def ask_model(ctx: interactions.SlashContext, model: str = "", prompt: str = "") -> None:
+  if model not in MODEL_CHOICES:
+    await ctx.send(f"Invalid model `{model}`. Please choose from `{MODEL_CHOICES}`.")
+    return
   await ctx.send(f'You asked model {model} with the prompt: "{prompt}"')
 
 
 @ask_model.autocomplete("model")
 async def autocomplete(ctx: interactions.AutocompleteContext) -> None:
-  string_option_input = ctx.input_text  # note: can be empty
-  print(f"input: {string_option_input}")
+  string_option_input = ctx.input_text
   # you can use ctx.kwargs.get("name") to get the current state of other options - note they can be empty too
   # make sure you respond within three seconds
 
-  choices = ["gpt3", "gpt4"]
-  filtered_choices = [choice for choice in choices if string_option_input in choice]
+  filtered_choices = [choice for choice in MODEL_CHOICES if string_option_input in choice]
 
   await ctx.send(
     choices=[
