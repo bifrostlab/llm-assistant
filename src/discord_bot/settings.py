bb6cd2abeefd06b4bf9a29f3291cb69394545fb4
Rodo <rodonguyendd@gmail.com>
2024-03-09 22:42:00 +1000

    chore: ruff formating

--- src/discord_bot/settings.py
+++ src/discord_bot/settings.py
@@ -1,10 +1,11 @@
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
   model_config = SettingsConfigDict(env_file="../../.env", env_file_encoding="utf-8")
 
   DISCORD_BOT_TOKEN: str
   AI_SERVER_URL: str = "http://localhost:8000"
+
 
 settings = Settings()
