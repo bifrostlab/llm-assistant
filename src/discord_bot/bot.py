724584b9168890af534919d4274d7c326584f92f
Rodo <rodonguyendd@gmail.com>
2024-03-21 12:45:21 +1000

    feat: new command to review resume, misc change to module path

--- src/discord_bot/bot.py
+++ src/discord_bot/bot.py
@@ -1,9 +1,11 @@
 import interactions
-from llm import answer_question
+import re
+import llm
 from settings import Settings
 
 
 MODEL_CHOICES = ["gpt-3.5-turbo", "gpt-4", "phi"]
+DEFAULT_MODEL = MODEL_CHOICES[2]
 DISCORD_BOT_TOKEN = Settings().DISCORD_BOT_TOKEN
 AI_SERVER_URL = Settings().AI_SERVER_URL
 
@@ -21,7 +23,7 @@ async def on_message_create(event: interactions.api.events.MessageCreate) -> Non
   print(f"Message received: {event.message.content}")
 
 
-@interactions.slash_command(name="ask", description="Ask an LLM")
+@interactions.slash_command(name="ask", description="Ask an LLM to answer anything")
 @interactions.slash_option(
   name="model",
   description="Choose an LLM model",
@@ -32,19 +34,19 @@ async def on_message_create(event: interactions.api.events.MessageCreate) -> Non
 @interactions.slash_option(
   name="prompt", description="Enter your prompt", required=True, opt_type=interactions.OptionType.STRING, min_length=10
 )
-async def ask_model(ctx: interactions.SlashContext, model: str = "", prompt: str = "") -> None:
+async def ask(ctx: interactions.SlashContext, model: str = "", prompt: str = "") -> None:
   if model not in MODEL_CHOICES:
     await ctx.send(f"Invalid model `{model}`. Please choose from `{MODEL_CHOICES}`.")
     return
 
   await ctx.defer()
 
-  response = await answer_question(model, prompt, AI_SERVER_URL)
+  response = await llm.answer_question(model, prompt, AI_SERVER_URL)
   for r in response:
     await ctx.send(r)
 
 
-@ask_model.autocomplete("model")
+@ask.autocomplete("model")
 async def autocomplete(ctx: interactions.AutocompleteContext) -> None:
   string_option_input = ctx.input_text
   # you can use ctx.kwargs.get("name") to get the current state of other options - note they can be empty too
@@ -61,6 +63,26 @@ async def autocomplete(ctx: interactions.AutocompleteContext) -> None:
       for choice in filtered_choices
     ]
   )
+
+
+@interactions.slash_command(name="review-resume", description="Ask an LLM to review a resume")
+@interactions.slash_option(
+  name="url",
+  description="A downloadable url of your resume",
+  required=True,
+  opt_type=interactions.OptionType.STRING,
+)
+async def review_resume(ctx: interactions.SlashContext, url: str = "") -> None:
+  # validate url structure, must have leading "http" and a domain name (e.g. "example.com"=`\w+\.\w+`)
+  if not re.search(r"http.+\w+\.\w+", url):
+    await ctx.send("Invalid URL. Please provide a valid URL of your resume.")
+    return
+
+  await ctx.defer()
+
+  response = await llm.review_resume(DEFAULT_MODEL, url, AI_SERVER_URL)
+  for r in response:
+    await ctx.send(r)
 
 
 def main() -> None:
