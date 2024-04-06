f83d7d3a6e1048580e3f48bc16c0c28ea63e9dbb
Rodo <rodonguyendd@gmail.com>
2024-04-06 22:44:25 +1000

    feat: make model choice optional for /ask command

--- src/discord_bot/bot.py
+++ src/discord_bot/bot.py
@@ -27,16 +27,16 @@ async def on_message_create(event: interactions.api.events.MessageCreate) -> Non
 
 @interactions.slash_command(name="ask", description="Ask an LLM to answer anything")
 @interactions.slash_option(
-  name="model",
-  description="Choose an LLM model",
-  required=True,
-  opt_type=interactions.OptionType.STRING,
-  autocomplete=True,
-)
-@interactions.slash_option(
   name="prompt", description="Enter your prompt", required=True, opt_type=interactions.OptionType.STRING, min_length=10
 )
-async def ask(ctx: interactions.SlashContext, model: str = "", prompt: str = "") -> None:
+@interactions.slash_option(
+  name="model",
+  description="Choose an LLM model",
+  required=False,
+  opt_type=interactions.OptionType.STRING,
+  choices=[interactions.SlashCommandChoice(name=model, value=model) for model in MODEL_CHOICES],
+)
+async def ask(ctx: interactions.SlashContext, prompt: str = "", model: str = DEFAULT_MODEL) -> None:
   if model not in MODEL_CHOICES:
     await ctx.send(f"Invalid model `{model}`. Please choose from `{MODEL_CHOICES}`.")
     return
@@ -46,25 +46,6 @@ async def ask(ctx: interactions.SlashContext, model: str = "", prompt: str = "")
   response = await llm.answer_question(model, prompt, AI_SERVER_URL)
   for r in response:
     await ctx.send(r)
-
-
-@ask.autocomplete("model")
-async def autocomplete(ctx: interactions.AutocompleteContext) -> None:
-  string_option_input = ctx.input_text
-  # you can use ctx.kwargs.get("name") to get the current state of other options - note they can be empty too
-  # make sure you respond within three seconds
-
-  filtered_choices = [choice for choice in MODEL_CHOICES if string_option_input in choice]
-
-  await ctx.send(
-    choices=[
-      {
-        "name": choice,
-        "value": choice,
-      }
-      for choice in filtered_choices
-    ]
-  )
 
 
 @interactions.slash_command(name="review-resume", description="Ask an LLM to review a resume")
