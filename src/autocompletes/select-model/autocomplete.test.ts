59c86edcb5eb7d5c973b9eb63c0817e24a5f94e7
Sam Huynh <samhwang2112.dev@gmail.com>
2024-04-17 13:30:43 +1000

    test: remove unrelated autocomplete test (#61)
    This parameter is controlled by the Discord UI. In no way it will reach our internal code, so testing it is pointless

--- src/autocompletes/select-model/autocomplete.test.ts
+++ src/autocompletes/select-model/autocomplete.test.ts
@@ -10,16 +10,6 @@ describe('autocomplete', () => {
     mockReset(mockAutocompleteInteraction);
   });
 
-  it("Should return nothing if the short term isn't long enough", async () => {
-    mockAutocompleteInteraction.options.getString.mockReturnValueOnce('solve');
-    const respondInput = captor<Parameters<AutocompleteInteraction['respond']>['0']>();
-
-    await selectModelAutocomplete(mockAutocompleteInteraction);
-
-    expect(mockAutocompleteInteraction.respond).toBeCalledWith(respondInput);
-    expect(respondInput.value.length).toEqual(0);
-  });
-
   it('Should return nothing if no options are found', async () => {
     mockAutocompleteInteraction.options.getString.mockReturnValueOnce('some random search that not existed');
     const respondInput = captor<Parameters<AutocompleteInteraction['respond']>['0']>();
